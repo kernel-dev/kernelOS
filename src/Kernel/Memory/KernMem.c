7a99b85c0ce1753552f6334bfdff0256eae256a3
kernel-dev <twitchisadeck@gmail.com>
2022-10-26 16:49:38 +0200

    add(KernMem): `KernMemset()` function

--- src/Kernel/Memory/KernMem.c
+++ src/Kernel/Memory/KernMem.c
@@ -2,3 +2,17 @@
 
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+
+VOID *
+KernMemset (
+    IN  VOID    *PTR,
+    IN  INT64   CH,
+    IN  INT64   Count
+)
+{
+    unsigned char *p = (unsigned char *) PTR;
+
+    for (INT64 i = 0; i < Count; ++i, ++p) *p = (unsigned char) CH;
+
+    return PTR;
+}
