9a037e671a9dd672bf684c3627d784e95b2c98e8
kernel-dev <twitchisadeck@gmail.com>
2022-10-26 14:24:11 +0200

    refactor(build): build app as PE32+ image binary
    - Do not build as UEFI application using EDK2's build system; instead use Clang and MingW64.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,38 @@
-# Move to EDK2 directory
-cd /Users/kernel/Documents/edk2-master/edk2/
+. /Users/kernel/Documents/edk2-oofer/edk2/BootloaderPkg/setup.sh
 
-# Source edksetup.sh to reconfigure config
-. edksetup.sh
+cd /Users/kernel/Documents/edk2-oofer/edk2/KernelOSPkg
 
-# Compile project
-cd KernelOSPkg
-build -a X64 -t XCODE5 -b RELEASE -p MdeModulePkg/MdeModulePkg.dsc || exit 1
+rm -rf kernel.o 2> /dev/null
+rm -rf hda-contents/kernel.bin* 2> /dev/null
 
-# Copy EFI binary
-cp ../Build/MdeModule/RELEASE_XCODE5/X64/KernelOSPkg/KernelOSPkg/OUTPUT/KernelOS.efi efi/KernelOS.efi
-cp ../Build/MdeModule/RELEASE_XCODE5/X64/KernelOSPkg/KernelOSPkg/OUTPUT/KernelOS.efi hda-contents/KernelOS.efi
+# Compile
+/usr/local/opt/llvm/bin/clang \
+    --sysroot=/Users/kernel/Downloads/mingw64/lib/gcc/x86_64-w64-mingw32 \
+    --target=x86_64-pc-windows-gnu \
+    -Wl,-e,KernMain \
+    -ffreestanding \
+    -fuse-ld="/usr/local/bin/x86_64-w64-mingw32-ld" \
+    -nostdlib \
+    \
+    -o kernel.o \
+    src/Kernel/Kernel.c \
+    src/Kernel/Memory/KernMem.c \
+    src/Kernel/Graphics/KernGraphics.c \
+    \
+    -I/Users/kernel/Documents/edk2-oofer/edk2/MdePkg/Include/ \
+    -I/Users/kernel/Documents/edk2-oofer/edk2/MdePkg/Include/X64 \
+    -I/Users/kernel/Documents/edk2-oofer/edk2/KernelOSPkg/src/Common \
+    -L/Users/kernel/Documents/edk2-oofer/edk2/MdePkg/Library/ \
+    \
+    -fshort-wchar \
+    || exit 1 # Exit if something goes wrong
 
-# Run the OS
-qemu-system-x86_64 -pflash bootloader/bios.bin -hda fat:rw:hda-contents -net none
+cp kernel.o hda-contents/kernel.bin 2> /dev/null
+
+# Run the OVMF.fd inside of QEMU
+# qemu-system-x86_64 -serial stdio -pflash bootloader/bios.bin -hda fat:rw:hda-contents -net none -m 2048M
+qemu-system-x86_64 \
+    -serial stdio \
+    -bios bootloader/bios.bin \
+    -drive file=fat:rw:hda-contents,format=raw,media=disk \
+    -m 4096M
