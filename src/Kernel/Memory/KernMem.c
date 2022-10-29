a392f70b1e5b3a991ba83ea6efabefdaf47b0bed
kernel-dev <twitchisadeck@gmail.com>
2022-10-29 03:51:39 +0200

    refactor(KernMem.c): use `while` loop instead of `for`

--- src/Kernel/Memory/KernMem.c
+++ src/Kernel/Memory/KernMem.c
@@ -1,6 +1,7 @@
 #include "../../Common/Memory/KernMem.h"
 
 #include <Uefi.h>
+
 #include <Library/UefiLib.h>
 
 VOID *
@@ -10,9 +11,15 @@ KernMemset (
     IN  INT64   Count
 )
 {
-    unsigned char *p = (unsigned char *) PTR;
+    unsigned char *Dest = PTR;
 
-    for (INT64 i = 0; i < Count; ++i, ++p) *p = (unsigned char) CH;
+    while (Count > 0)
+    {
+        *Dest = (unsigned char) CH;
+
+        Dest++;
+        Count--;
+    }
 
     return PTR;
 }
