10ad689a250acc542dd97e200c09d5bb16221400
kernel-dev <twitchisadeck@gmail.com>
2022-10-29 03:50:47 +0200

    add(KernMem.h): proper documentation comments

--- src/Common/Memory/KernMem.h
+++ src/Common/Memory/KernMem.h
@@ -31,7 +31,7 @@ typedef struct {
     void        *Buffer;
     
     ///
-    /// Whether or not this page is available for use.
+    /// Whether or not this page is available for use
     ///
     BOOLEAN     Available;
 } KERNEL_VIRTUAL_MEMORY_PAGE;
@@ -53,6 +53,16 @@ typedef struct {
     UINT64                      *PhysicalAddress;
 } KERNEL_PHYSICAL_MEMORY_PAGE;
 
+/**
+    Fills the provided memory blocks
+    with the value of CH for Count bytes.
+
+    @param[in]  PTR     The pointer to the base address of the location.
+    @param[in]  CH      The value to fill the PTR with.
+    @param[in]  Count   The size of CH in bytes.
+
+    @retval     VOID*   The modified pointer.
+ **/
 VOID *
 KernMemset (
     IN  VOID    *PTR,
