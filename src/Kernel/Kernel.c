d3030bcc0ebda62928f56a9788f2284b977f6515
kernel-dev <twitchisadeck@gmail.com>
2022-11-01 20:32:39 +0100

    add(Kernel.c): clear screen and write yellow pixels to the entirety of it

--- src/Kernel/Kernel.c
+++ src/Kernel/Kernel.c
@@ -14,10 +14,27 @@ VOID
 KernMain (
     IN EFI_RUNTIME_SERVICES                         *RT,
     IN EFI_KERN_MEMORY_MAP                          *MemoryMap,
-    IN ACPI_DIFFERENTIATED_SYSTEM_DESCRIPTOR_TABLE  *Dsdt,
+    IN ACPI_DIFFERENTIATED_SYSTEM_DESCRIPTOR_TABLE  **Dsdt,
     IN KERN_FRAMEBUFFER                             *Framebuffer)
 {
+
+    //
+    //  Clear the screen.
+    //
     ScreenClearTerminal (Framebuffer);
+
+    //
+    //  Fills the entire screen with
+    //  bright yellow pixels.
+    //
+    ScreenFillRectangle (
+        Framebuffer,
+        0,
+        0,
+        Framebuffer->HorizontalRes * Framebuffer->PPS,
+        Framebuffer->VerticalRes * Framebuffer->PPS,
+        0xFFFFFF5F);
+
     //
     //  Should never reach here.
     //  Will be removed later.
