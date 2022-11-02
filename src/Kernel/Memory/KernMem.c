68d7ab9c041e891a02bb9603ebffbc34650f3e48
kernel-dev <twitchisadeck@gmail.com>
2022-11-02 06:03:45 +0100

    impl(KernMem.c): various `memset` functions

--- src/Kernel/Memory/KernMem.c
+++ src/Kernel/Memory/KernMem.c
@@ -5,17 +5,68 @@
 #include <Library/UefiLib.h>
 
 VOID *
-KernMemset (
-    IN  VOID    *PTR,
-    IN  INT64   CH,
-    IN  INT64   Count
+KernMemset32 (
+    IN  VOID        *PTR,
+    IN  INT32       Value,
+    IN  UINT32      Count
 )
 {
-    unsigned char *Dest = PTR;
+    UINT32 *Dest = PTR;
 
     while (Count--)
     {
-        *Dest++ = (unsigned char) CH;
+        *Dest++ = (UINT32) Value;
+    }
+
+    return PTR;
+}
+
+VOID *
+KernMemset64 (
+    IN  VOID        *PTR,
+    IN  INT64       Value,
+    IN  UINT64      Count
+)
+{
+    UINT64 *Dest = PTR;
+
+    while (Count--)
+    {
+        *Dest++ = (UINT64) Value;
+    }
+
+    return PTR;
+}
+
+VOID *
+VolatileKernMemset32 (
+    IN  VOID        *PTR,
+    IN  INT32       Value,
+    IN  UINT32      Count
+)
+{
+    volatile UINT32 *Dest = PTR;
+
+    while (Count--)
+    {
+        *Dest++ = (UINT32) Value;
+    }
+
+    return PTR;
+}
+
+VOID *
+VolatileKernMemset64 (
+    IN  VOID        *PTR,
+    IN  INT64       Value,
+    IN  UINT64      Count
+)
+{
+    volatile UINT64 *Dest = PTR;
+
+    while (Count--)
+    {
+        *Dest++ = (UINT64) Value;
     }
 
     return PTR;
