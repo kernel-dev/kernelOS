5b015b67e0d860f1ff9e67a6b34828376c8c67d9
kernel-dev <twitchisadeck@gmail.com>
2022-11-05 18:31:19 +0100

    update main file

--- src/Kernel/Kernel.c
+++ src/Kernel/Kernel.c
@@ -1,7 +1,11 @@
 #include "../Common/Acpi/KernEfiAcpi.h"
+#include "../Common/Assert/KernAssert.h"
 #include "../Common/Memory/KernEfiMem.h"
+#include "../Common/Memory/KernMem.h"
 #include "../Common/Graphics/KernGop.h"
 #include "../Common/Graphics/KernGraphics.h"
+#include "../Common/Graphics/KernFontParser.h"
+#include "../Common/Util/KernRuntimeValues.h"
 
 #include <Uefi.h>
 
@@ -15,25 +19,25 @@ KernMain (
     IN EFI_RUNTIME_SERVICES                         *RT,
     IN EFI_KERN_MEMORY_MAP                          *MemoryMap,
     IN ACPI_DIFFERENTIATED_SYSTEM_DESCRIPTOR_TABLE  **Dsdt,
-    IN KERN_FRAMEBUFFER                             *Framebuffer)
+    IN KERN_FRAMEBUFFER                             *Framebuffer,
+    IN VOID                                         *TerminalFont)
 {
+    FB = Framebuffer;
+    FontFile = TerminalFont;
 
     //
     //  Clear the screen.
     //
-    ScreenClearTerminal (Framebuffer);
+    ScreenClearTerminal ();
 
-    //
-    //  Fills the entire screen with
-    //  bright yellow pixels.
-    //
-    ScreenFillRectangle (
-        Framebuffer,
-        0,
-        0,
-        Framebuffer->HorizontalRes,
-        Framebuffer->VerticalRes,
-        0xFFFFFF5F);
+    PSFInit (
+        TerminalFont,
+        &FontHdr,
+        &ExtFontHdr,
+        TRUE);
+
+    kprint ("Hello, world!\n");
+
 
     //
     //  Should never reach here.
